76aa103b18b6cc7090ffbcf55c6ea605ce90782f
liuqiang187 <409864676@qq.com>
2016-07-22 15:27:25 +0800

    add Kingfisher

--- swiftTaste/Controller/SecondViewController.swift
+++ swiftTaste/Controller/SecondViewController.swift
@@ -20,7 +20,28 @@ class SecondViewController: UIViewController {
 //        view.backgroundColor = RGB(40, G: 162, B: 100)
         view.backgroundColor = UIColor.whiteColor()
         
+        setup()
+    }
+    
+    private func setup()
+    {
+        view.addSubview(inputBtn)
+        inputBtn.snp_makeConstraints { (make) in
+            make.centerX.equalTo(view.snp_centerX)
+            make.centerY.equalTo(view.snp_centerY)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
     }
 
+    // MARK: - lazy var
+    private lazy var inputBtn : UIButton = {
+        let btn = UIButton()
+        btn.setTitle("摇一摇", forState: .Normal)
+        btn.backgroundColor = MainColor()
+        btn.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        btn.addTarget(self, action: #selector(FirstViewController.btnClick), forControlEvents: .TouchUpInside)
+        return btn
+    }()
 
 }
