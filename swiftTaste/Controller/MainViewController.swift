f84d6a6104f0200d2925d1fa567aadc75c8c6378
liuqiang187 <409864676@qq.com>
2016-07-25 17:25:30 +0800

    add discoverVC

--- swiftTaste/Controller/MainViewController.swift
+++ swiftTaste/Controller/MainViewController.swift
@@ -24,7 +24,7 @@ class MainViewController: UITabBarController, UITabBarControllerDelegate {
         
         addViewController(FirstViewController(), title: "chat", icon: "tabbar_mainframe", iconPress: "tabbar_mainframeHL")
         addViewController(SecondViewController(), title: "friend", icon: "tabbar_contacts", iconPress: "tabbar_contactsHL")
-        addViewController(ThirdViewController(), title: "hi", icon: "tabbar_discover", iconPress: "tabbar_discoverHL")
+        addViewController(DiscoverViewController(), title: "hi", icon: "tabbar_discover", iconPress: "tabbar_discoverHL")
         addViewController(MeMainViewController(), title: "me", icon: "tabbar_me", iconPress: "tabbar_meHL")
     }
     
